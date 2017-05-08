569e5e625d7a15dd528f1a7367352e3222b8a5a2
liyong <liyong19861014@gmail.com>
2017-05-08 16:19:28 +0800

    整理README.md

--- makedoc.sh
+++ makedoc.sh
@@ -5,7 +5,7 @@ CMD=$(which pandoc)
 if [[ -z ${CMD} ]]; then
     echo "请安装pandoc"
 else
-    pandoc -f markdown -f rst -o README.rst README.md
+    pandoc -f markdown -f rst -o README.txt README.md
 fi
 
 
--- setup.py
+++ setup.py
@@ -15,16 +15,25 @@ if sys.platform == 'darwin':
 
 extra_compile_args = ['-g', '-fPIC', '-Wall', '-O2']
 
-VERSION = '0.1.3'
+VERSION = '0.1.4'
 
 if not os.path.exists('smhasher/src'):
     sys.stderr.write('run command:\ngit submodule update --init\n')
-    p = subprocess.Popen(args=['git','submodule','update','--init'],
-            stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    p = subprocess.Popen(args=['git', 'submodule', 'update', '--init'],
+                         stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     outs, errs = p.communicate()
     p.wait()
     sys.stderr.write(outs)
     sys.stderr.write(errs)
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+readme_path = os.path.join(here, 'README.md')
+try:
+    import pypandoc
+    long_description = pypandoc.convert(readme_path, 'rst')
+except (IOError, ImportError):
+    long_description = open(readme_path).read()
 
 cpp_files = [
     'smhasher/src/MurmurHash2.cpp',
@@ -43,10 +52,37 @@ pysmhasher_ext = Extension(
 setup(
     name='pysmhasher',
     version=VERSION,
-    maintainer='Yong Li',
-    maintainer_email='liyong19861014@gmail.com',
+    author='Yong Li',
+    author_email='liyong19861014@gmail.com',
     url='https://github.com/airhuman/pysmhasher.git',
     description='Python Extensions for SMHasher',
-    keywords=['hash', 'smhasher'],
-    ext_modules=[pysmhasher_ext]
+    long_description=long_description,
+    keywords='hash hashing smhasher',
+    license='MIT',
+    ext_modules=[pysmhasher_ext],
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: MIT License',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
 )
