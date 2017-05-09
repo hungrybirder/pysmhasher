997c26f17f3b4c4fb0cde0b3a8d80769ea725ba3
liyong <liyong19861014@gmail.com>
2017-05-09 13:39:06 +0800

    traivs deploy

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ if sys.platform == 'darwin':
 
 extra_compile_args = ['-g', '-fPIC', '-Wall', '-O2']
 
-VERSION = '0.1.7'
+VERSION = '0.1.8'
 
 if not os.path.exists('smhasher/src'):
     sys.stderr.write('run command:\ngit submodule update --init\n')
