49de9cdbc67e24335ed7e3eb05f746a3316fb1be
liyong <hungrybirder@gmail.com>
2019-07-15 11:29:38 +0800

    test_pysmhasher.py

--- tests/test_pysmhasher.py
+++ tests/test_pysmhasher.py
@@ -8,3 +8,7 @@ import pysmhasher
 def test_murmurhash2():
     key = 'abc'
     assert pysmhasher.murmurhash2(key) == 324500635
+
+def test_cityhash64():
+    key = 'DBE4CBA7-2ADC-4F32-8D54-D8F065FED9CC'
+    assert pysmhasher.cityhash64(key) == 6587470800993669582
